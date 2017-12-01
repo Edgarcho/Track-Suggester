7d844db5a2e6074125dd18f6cf13402e634f859a
Edgar Hernandez <edgarchortiz@gmail.com>
2017-12-01 15:28:20 -0800

    add function and variable to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 $(document).ready(function(){
   $("form#quiz").submit(function(event){
     event.preventDefault();
-    var question1 = $("input:radio[name=question1]checked").val();
-    var question2 = $("input:radio[name=question2]checked").val();
-    var question3 = $("input:radio[name=question3]checked").val();
+    var question1 = $("input:radio[name=question1]:checked").val();
+    var question2 = $("input:radio[name=question2]:checked").val();
+    var question3 = $("input:radio[name=question3]:checked").val();
+    var result;
+    if (question1 === "google" && question2 === "enterp" && question3 ==="moblie"){
+      $("#result").show();
 
-    $("#result").show();
+    }else {
+      $("#career2").show();
+    }
+
+
 
   });
 });
