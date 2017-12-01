1af186c05f81f0276076c94f1f01c796d7b18615
Edgar Hernandez <edgarchortiz@gmail.com>
2017-12-01 15:00:57 -0800

    add var to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
   $("form#quiz").submit(function(event){
     event.preventDefault();
-    alert("Please try again.");
+    var question1 = $("input:radio[name=question1]checked").val();
+    var question2 = $("input:radio[name=question2]checked").val();
+    var question3 = $("input:radio[name=question3]checked").val();
+
+    $("#result").show();
+
   });
 });
