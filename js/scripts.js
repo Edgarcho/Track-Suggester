e61edcddcc3ac64060b265ce6cb556e983d12f22
Edgar Hernandez <edgarchortiz@gmail.com>
2017-12-01 16:44:14 -0800

    add else if statement

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,24 @@ $(document).ready(function(){
     var question1 = $("input:radio[name=question1]:checked").val();
     var question2 = $("input:radio[name=question2]:checked").val();
     var question3 = $("input:radio[name=question3]:checked").val();
-    var result;
-    if (question1 === "google" && question2 === "enterp" && question3 ==="moblie"){
+    var answer1 = (question1+question2+question3);
+    if (answer1 ==="googleenterpmoblie" ){
       $("#career").show();
       $("#career1").hide();
       $("#career2").hide();
-
-    }else {
+      $("form#quiz").submit();
+    }else if (answer1 ==="microsoftbusinessdesktop"){
+      $("#career2").show();
+      $("#career").hide();
+      $("#career1").hide();
+      $("form#quiz").submit();
+    }else if (answer1 ==="newrelicstartweb"){
       $("#career1").show();
       $("#career").hide();
       $("#career2").hide();
-
+      $("form#quiz").submit();
+    }else {
+     $("form#quiz").submit();
     }
-
-
-
   });
 });
